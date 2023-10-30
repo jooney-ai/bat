07126fcf2f5da70800563f306d1b1345a15a4ed1
Joon <115054844+jooney-ai@users.noreply.github.com>
2023-10-31 00:37:04 +0900

    Create install_anaconda.sh

---
+++ linux/install_anaconda.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# install anaconda
+wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh
+
+# run sh
+./Anaconda3-2023.09-0-Linux-x86_64.sh
+
+# create vitual environment
+conda create --name name --yes
+
+# activate
+conda activate name
