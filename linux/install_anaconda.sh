8b70ad2e4f433863e11dffb4806cc57d9a73bd04
Joon <115054844+jooney-ai@users.noreply.github.com>
2023-10-31 09:44:11 +0900

    Update install_anaconda.sh

--- linux/install_anaconda.sh
+++ linux/install_anaconda.sh
@@ -3,6 +3,9 @@
 # install anaconda
 wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh
 
+# authority
+chmod +x Anaconda3-2023.09-0-Linux-x86_64.sh
+
 # run sh
 ./Anaconda3-2023.09-0-Linux-x86_64.sh
 
